5be4af72d5bd6a48be60e7dac3ba7c1558167b7f
Sumit Awale <sumit.awale@ebpearls.com>
2024-07-15 16:00:11 +0545

    add: add faq page

--- src/menu-items/index.tsx
+++ src/menu-items/index.tsx
@@ -6,11 +6,12 @@ import reports from './reports';
 // types
 import { NavItemType } from 'types';
 import settingPage from './setting';
+import faqPage from './faq';
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems: { items: NavItemType[] } = {
-  items: [samplePage, pages, reports, settingPage]
+  items: [samplePage, pages, reports, faqPage, settingPage]
 };
 
 export default menuItems;
