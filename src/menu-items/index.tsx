2c982db6d1f916369696bbd4e55c87f3e1a7b4a3
Sumit Awale <sumit.awale@ebpearls.com>
2024-07-23 14:31:15 +0545

    fix: remove unnecessary menus from menu list from dashboard

--- src/menu-items/index.tsx
+++ src/menu-items/index.tsx
@@ -6,28 +6,11 @@ import reports from './reports';
 // types
 import { NavItemType } from 'types';
 import settingPage from './setting';
-import aboutUsPage from './about-us';
-import faqPage from './faq';
-import privacyPolicyPage from './privacy-policy';
-import termsConditionsPage from './terms-conditions';
-import cookiesPolicyPage from './cookies-policy';
-import homePage from './homepage';
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems: { items: NavItemType[] } = {
-  items: [
-    homePage,
-    samplePage,
-    aboutUsPage,
-    faqPage,
-    cookiesPolicyPage,
-    privacyPolicyPage,
-    termsConditionsPage,
-    pages,
-    reports,
-    settingPage
-  ]
+  items: [samplePage, pages, reports, settingPage]
 };
 
 export default menuItems;
