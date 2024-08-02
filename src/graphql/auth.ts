0f13923524cde22078d62a93603b4f32f261cb79
Sumit Awale <sumit.awale@ebpearls.com>
2024-08-02 10:17:44 +0545

    update: update mutation of facebook and google

--- src/graphql/auth.ts
+++ src/graphql/auth.ts
@@ -41,13 +41,9 @@ export const LOGIN_MUTATION = gql`
 `;
 
 export const FACEBOOK_SIGNIN_MUTATION = gql`
-  mutation loginWithFacebook($body: LoginFacebookInput!) {
-    loginWithFacebook(body: $body) {
+  mutation LoginWithFacebook($accessToken: String!, $deviceId: String!) {
+    loginWithFacebook(accessToken: $accessToken, deviceId: $deviceId) {
       message
-      expiry {
-        expiresBy
-        expiresAt
-      }
       token {
         accessToken
         accessTokenExpiresIn
@@ -56,21 +52,18 @@ export const FACEBOOK_SIGNIN_MUTATION = gql`
       }
       user {
         _id
+        cometAuthToken
         email
-        status
+        loginType
       }
     }
   }
 `;
 
 export const GOOGLE_SIGNIN_MUTATION = gql`
-  mutation loginWithGoogle($body: GoogleLoginInput!) {
-    loginWithGoogle(body: $body) {
+  mutation LoginWithGoogle($idToken: String!, $deviceId: String!) {
+    loginWithGoogle(idToken: $idToken, deviceId: $deviceId) {
       message
-      expiry {
-        expiresBy
-        expiresAt
-      }
       token {
         accessToken
         accessTokenExpiresIn
@@ -79,8 +72,9 @@ export const GOOGLE_SIGNIN_MUTATION = gql`
       }
       user {
         _id
+        cometAuthToken
         email
-        status
+        loginType
       }
     }
   }
