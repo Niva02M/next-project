cad62bf5fdcaf5d9d68f1c965d10276dde131c67
Sumit Awale <sumit.awale@ebpearls.com>
2024-09-11 18:03:51 +0545

    layout: stripe add card layout has been added

--- src/views/settings/payment/AddPayementElement.tsx
+++ src/views/settings/payment/AddPayementElement.tsx
@@ -1,6 +1,6 @@
-// import { Elements } from '@stripe/react-stripe-js';
-// import { loadStripe } from '@stripe/stripe-js';
-// import CheckoutForm from './CheckoutForm';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import CheckoutForm from './CheckoutForm';
 import { Button, Typography } from '@mui/material';
 import { GET_EPHEMERAL_KEY_QUERY } from './graphql/queries';
 import { useQuery } from '@apollo/client';
@@ -8,38 +8,31 @@ import { useEffect, useState } from 'react';
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-// const stripePromise = loadStripe(
-//   'pk_test_51Mc0MNEr2SjM3rR4LaEDHNLk03VqGvBNMIOOQ4Khtyo2gS5mJp0nMAiVPPeianRFTJcXiPVJZitbibx2KJQU73r500F7Rpyih7'
-// );
+const stripePromise = loadStripe(
+  'pk_test_51Mc0MNEr2SjM3rR4LaEDHNLk03VqGvBNMIOOQ4Khtyo2gS5mJp0nMAiVPPeianRFTJcXiPVJZitbibx2KJQU73r500F7Rpyih7'
+);
 
 export default function AddPaymentElement({ kind }: { kind: string }) {
   const { data } = useQuery(GET_EPHEMERAL_KEY_QUERY);
-  // console.log('stripePromise ====>', stripePromise);
   const [secret, setSecret] = useState('');
-  console.log('data ====>', data);
-  // `${resData.id}_secret_${resData.clientSecret}`
   useEffect(() => {
     if (data?.getEphemeralKey) setSecret(`${data?.getEphemeralKey?.data?.keyId}_secret_${data?.getEphemeralKey?.data?.keySecret}`);
-    console.log('options -===>', secret);
   }, [data?.getEphemeralKey]);
+  const options = {
+    mode: 'payment',
+    amount: 1099,
+    currency: 'usd',
+    // Fully customizable with appearance API.
+    appearance: {
+      /*...*/
+    }
+  };
+
   return secret ? (
     <>
-      {/*<Elements stripe={stripePromise} options={{ clientSecret: secret }}>
+      <Elements stripe={stripePromise} options={options}>
         <CheckoutForm />
-      </Elements> */}
-      <Button
-        onClick={(e) => {
-          e.preventDefault();
-          // save(stripe, elements);
-        }}
-        type="submit"
-        variant="contained"
-        color="secondary"
-        fullWidth
-        // disabled={!stripe || savePaymemtMethodLoadng}
-      >
-        Add payment details
-      </Button>
+      </Elements>
     </>
   ) : null;
 }
--- src/views/settings/payment/CheckoutForm.tsx
+++ src/views/settings/payment/CheckoutForm.tsx
@@ -1,10 +1,23 @@
+import { Button } from '@mui/material';
 import {PaymentElement} from '@stripe/react-stripe-js';
 
 const CheckoutForm = () => {
   return (
     <form>
       <PaymentElement />
-      <button>Submit</button>
+      <Button
+        onClick={(e) => {
+          e.preventDefault();
+          // save(stripe, elements);
+        }}
+        type="submit"
+        variant="contained"
+        color="secondary"
+        fullWidth
+        // disabled={!stripe || savePaymemtMethodLoadng}
+      >
+        Add payment details
+      </Button>
     </form>
   );
 };
